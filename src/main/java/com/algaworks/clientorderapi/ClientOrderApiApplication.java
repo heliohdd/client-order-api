3eac2c108d6755fb8562697d199c5e70df7c8ea9
Helio Dourado <heliohdd@yahoo.com.br>
2024-04-03 10:50:05 -0300

    Commit inicial

---
+++ src/main/java/com/algaworks/clientorderapi/ClientOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.clientorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/clientorderapi/ClientOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.clientorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
